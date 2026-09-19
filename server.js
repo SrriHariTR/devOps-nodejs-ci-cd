a9b35d22c24254ba441527f067dd04804feb5606
srrihari <srriharitr@gmail.com>
2026-09-19 12:41:28 +0530

    Initial Node.js REST API

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require("express");
+
+const app = express();
+const PORT = 3000;
+
+app.get("/status", (req, res) => {
+  res.json({
+    status: "Application is running",
+  });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
